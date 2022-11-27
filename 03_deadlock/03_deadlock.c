8f7fa651c575a8de46ea29e5b84262282e443399
Bojan Piskulic <bpiskulic1996@gmail.com>
2022-11-27 18:48:04 +0100

    Deadloop example

---
+++ 03_deadlock/03_deadlock.c
@@ -0,0 +1,47 @@
+/************************************************************************
+Example 03: Deadloop example
+Show how to create deadloop using a MPI_Send() and MPI_Recv() routines.
+************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+    /* Init */
+    MPI_Init(&argc, &argv);
+
+    /* Main part */
+    int size, rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        int numberToReceive;
+        int numberToSend = 10;
+
+        printf("RANK {%d}: Receiving a value from {%d}.\n", rank, 1);
+        MPI_Recv(&numberToReceive, 1, MPI_INT, 1, 1991, MPI_COMM_WORLD, NULL);
+        printf("RANK {%d}: Received a value %d.\n", rank, numberToReceive);
+
+        printf("RANK {%d}: Sending a value %d.\n", rank, numberToSend);
+        MPI_Send(&numberToSend, 1, MPI_INT, 1, 2991, MPI_COMM_WORLD);
+    }
+
+    if (rank == 1) {
+        int numberToReceive;
+        int numberToSend = 100;
+
+        printf("RANK {%d}: Receiving a value from {%d}.\n", rank, 0);
+        MPI_Recv(&numberToReceive, 1, MPI_INT, 0, 1991, MPI_COMM_WORLD, NULL);
+        printf("RANK {%d}: Received a value %d.\n", rank, numberToReceive);
+
+
+        printf("RANK {%d}: Sending a value %d.\n", rank, numberToSend);
+        MPI_Send(&numberToSend, 1, MPI_INT, 0, 2991, MPI_COMM_WORLD);
+    }
+
+    /* Finalize */
+    MPI_Finalize();
+}
