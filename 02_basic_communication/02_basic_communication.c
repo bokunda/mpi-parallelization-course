d2ab6941d3f98635e5f9a9f20bb5070f8f5ff7b2
Bojan Piskulic <bpiskulic1996@gmail.com>
2022-11-27 18:31:57 +0100

    Basic communication example

---
+++ 02_basic_communication/02_basic_communication.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+    /* Init */
+    MPI_Init(&argc, &argv);
+
+    /* Main part */
+    int size, rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /* Simulate broadcast, rank 0 sends a message to all recipients. */
+    /* Generate random number and multiply it by recipient rank */
+    if (rank == 0)
+    {
+        int number = 10;
+        for (int i = 1; i < size; i++)
+        {
+            int numberToSend = number * i;
+            printf("RANK {%d}: Sending a value %d.\n", rank, numberToSend);
+            MPI_Send(&numberToSend, 1, MPI_INT, i, 1991, MPI_COMM_WORLD);
+        }
+    }
+
+    /* Receive a value */
+    if (rank != 0)
+    {
+        int receivedNumber;
+        MPI_Recv(&receivedNumber, 1, MPI_INT, 0, 1991, MPI_COMM_WORLD, NULL);
+        printf("RANK {%d}: Received a value %d.\n", rank, receivedNumber);
+    }
+
+    /* Finalize */
+    MPI_Finalize();
+
+    return 0;
+}
